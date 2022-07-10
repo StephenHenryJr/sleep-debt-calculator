15df03df72041bb583756bdabbb1bfbde334d764
StephenHenryJr <shenryjr_85@hotmail.com>
2022-07-10 18:34:33 -0400

    JS Dom event added to button to fire off function

--- main.js
+++ main.js
@@ -1,35 +1,65 @@
+// let input = document.getElementById('input');
+// let button = document.getElementById('button')
+// let responseField = document.getElementById('responseField')
+
+// //Function that very simply adds up daily sleep in hr(s) from M-S.
+// const getActualSleepHours = (hours) => {
+//   return hours * 7;
+// }
+
+// //Function that determines the weekly ideal amount of sleeping hours.
+// const getIdealSleepHours = idealHours => idealHours * 7;
+
+// //Function that calculates and determines if you got surplus, deficiency of perfect amount of sleep. Previous 2 functions are stored in variable and then used in if statement to make this determination.
+// const calculateSleepDebt = () => {
+//   const actualSleepHours = getActualSleepHours(8);
+//   const idealSleepHours = getIdealSleepHours(8);
+//   if (actualSleepHours === idealSleepHours) {
+//     console.log('You got the perfect amount of sleep!');
+//   }
+//   else if (actualSleepHours > idealSleepHours) {
+//     console.log('You got ' + (actualSleepHours - idealSleepHours) + ' hr(s) of extra sleep. Go burn some energy!');
+//   }
+//   else if (actualSleepHours < idealSleepHours) {
+//     console.log('You got ' + (idealSleepHours - actualSleepHours) + ' hr(s) less sleep than needed. You should get some rest!') }
+//   else {
+//       console.log('Error! Somehting went Wrong!');
+//     }
+//   }
+
+// calculateSleepDebt();
+
+let input = document.getElementById('input');
+let button = document.getElementById('button')
+let responseField = document.getElementById('responseField')
 
 //Function that very simply adds up daily sleep in hr(s) from M-S.
 const getActualSleepHours = (hours) => {
   return hours * 7;
 }
-console.log(getActualSleepHours(8));
 
 //Function that determines the weekly ideal amount of sleeping hours.
 const getIdealSleepHours = idealHours => idealHours * 7;
-console.log(getIdealSleepHours(8));
 
 //Function that calculates and determines if you got surplus, deficiency of perfect amount of sleep. Previous 2 functions are stored in variable and then used in if statement to make this determination.
 const calculateSleepDebt = () => {
-  const actualSleepHours = getActualSleepHours(8);
-
+  const actualSleepHours = getActualSleepHours(input.value);
   const idealSleepHours = getIdealSleepHours(8);
-
   if (actualSleepHours === idealSleepHours) {
     console.log('You got the perfect amount of sleep!');
+    responseField.innerHTML = 'You got the perfect amount of sleep!'
   }
-
   else if (actualSleepHours > idealSleepHours) {
     console.log('You got ' + (actualSleepHours - idealSleepHours) + ' hr(s) of extra sleep. Go burn some energy!');
+    responseField.innerHTML = 'You got ' + (actualSleepHours - idealSleepHours) + ' hr(s) of extra sleep. Go burn some energy!';
   }
-
   else if (actualSleepHours < idealSleepHours) {
-    console.log('You got ' + (idealSleepHours - actualSleepHours) + ' hr(s) less sleep than needed. You should get some rest!') }
-
+    console.log('You got ' + (idealSleepHours - actualSleepHours) + ' hr(s) less sleep than needed. You should get some rest!') 
+    responseField.innerHTML = 'You got ' + (idealSleepHours - actualSleepHours) + ' hr(s) less sleep than needed. You should get some rest!';
+  }
   else {
-      console.log('Error! Somehting went Wrong!');
+      console.log('Error! Something went Wrong!');
     }
-
   }
 
-calculateSleepDebt();
+button.addEventListener('click', calculateSleepDebt)
